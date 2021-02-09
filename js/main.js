42dfafc4af4c827517335badbd15b41e8389b4bd
Mohamed Nasser <mohamed.nasser3010@gmail.com>
2021-02-09 04:20:34 +0200

    add validation fixed

--- js/main.js
+++ js/main.js
@@ -1,48 +1,70 @@
 var form = document.getElementById('contact-form');
-var submit = document.getElementById('submit');
+var submit = document.getElementById('submit_form');
 var email_format = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.(?:[a-zA-Z0-9-]+)+$/ ;
 
-submit.click = function(e){
+
+submit.onclick = function(e){
     e.preventDefault();
     var fName = document.getElementById('FName');
     var lName = document.getElementById('LName');
     var email = document.getElementById('email');
     var phone = document.getElementById('phone');
     var msg = document.getElementById('msg');
+    var fname_error = false;
+    var lname_error = false;
+    var phone_error = false;
+    var email_error = false;
+    var msg_error = false;
     
-    var errors = false ;
 
     if(fName.value.length < 3 ){
         fName.classList.add('is-invalid');
         document.getElementById('FName_err').innerHTML = 'name must be at least 3 characters';
-        errors = true;
+        fname_error = true
+    }else{
+        fName.classList.remove('is-invalid');
+        document.getElementById('FName_err').innerHTML = '';
     }
 
     if(lName.value.length < 3 ){
         lName.classList.add('is-invalid');
         document.getElementById('LName_err').innerHTML = 'name must be at least 3 characters';
-        errors = true;
+        lname_error = true;
+    }else{
+        lName.classList.remove('is-invalid');
+        document.getElementById('LName_err').innerHTML = '';
     }
+
+
 
     if( !email_format.test(email.value)){
         email.classList.add('is-invalid');
         document.getElementById('email_err').innerHTML = 'email must be valid';
-        errors = true;
+        email_error = true;
+    }else{
+        email.classList.remove('is-invalid');
+        document.getElementById('email_err').innerHTML = ''; 
     }
 
     if(phone.value.length < 11 ){
         phone.classList.add('is-invalid');
         document.getElementById('phone_err').innerHTML = 'phone must be at least 11 characters';
-        errors = true;
+        phone_error = true;
+    }else{
+        phone.classList.remove('is-invalid');
+        document.getElementById('phone_err').innerHTML = '';
     }
 
     if(msg.value.length < 5 ){
         msg.classList.add('is-invalid');
         document.getElementById('msg_err').innerHTML = 'message must be at least 5 characters';
-        errors = true;
+        msg_error = true;
+    }else{
+        msg.classList.remove('is-invalid');
+        document.getElementById('msg_err').innerHTML = '';
     }
 
-    if(!errors){
+    if(!fname_error && !lname_error && !email_error && !phone_error && !msg_error){
         form.submit();  
    }
 }
